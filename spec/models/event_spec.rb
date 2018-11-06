4854123c738b1c0547b0a3b9e3689f09cd8193d9
Patrick Baselier <patrickbaselier@Patricks-MacBook-Pro.local>
2018-11-06 22:25:45 +0100

    Add test

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -12,6 +12,40 @@ RSpec.describe Event, type: :model do
     it { is_expected.to have_many(:users).through(:guests) }
   end
 
+  describe 'scopes' do
+
+    describe '.upcoming' do
+      subject { described_class.upcoming }
+
+      let!(:past) { create :event, scheduled_at: Date.yesterday }
+      let!(:today) { create :event, scheduled_at: Chronic.parse('today noon') }
+      let!(:tomorrow) { create :event, scheduled_at: Date.tomorrow }
+      let!(:far_away) { create :event, scheduled_at: 10.days.from_now }
+
+      it { is_expected.to contain_exactly(today, tomorrow, far_away) }
+    end
+
+    describe '.for_user' do
+      subject { described_class.for_user(me) }
+
+      let(:me) { create :user, name: 'me' }
+      let(:other) { create :user, name: 'other' }
+      let(:with_me) do
+        create :event do |e|
+          e.users << me
+        end
+      end
+      let(:without_me) do
+        create :event do |e|
+          e.users << other
+        end
+      end
+
+      it { is_expected.to contain_exactly(with_me) }
+    end
+  end
+
+
   describe '#scheduled_at' do
 
   end
