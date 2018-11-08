1fbc72ec03a6e43101ee1222bd8f2e825fc3cd07
Patrick Baselier <patrickbaselier@patricks-mbp.eu.asml.com>
2018-11-08 08:29:51 +0100

    Replace auto_eager_path with autload_path

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,6 @@ module Regale
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
     config.time_zone = 'Amsterdam'
-    config.eager_load_paths << Rails.root.join('lib')
+    config.autoload_paths << Rails.root.join('lib')
   end
 end
