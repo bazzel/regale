1cc3eb14deb8a549da6fa63b6d0830c70ffbfd41
Patrick Baselier <patrickbaselier@Patricks-MacBook-Pro.local>
2018-11-11 21:38:48 +0100

    Extract duplicate logic

--- app/decorators/event_decorator.rb
+++ app/decorators/event_decorator.rb
@@ -1,15 +1,6 @@
 class EventDecorator < ApplicationDecorator
   delegate_all
 
-  # Define presentation-specific methods here. Helpers are accessed through
-  # `helpers` (aka `h`). You can override attributes, for example:
-  #
-  #   def created_at
-  #     helpers.content_tag :span, class: 'time' do
-  #       object.created_at.strftime("%a %m/%d/%y")
-  #     end
-  #   end
-
   def calendar
     h.capture do
       h.concat scheduled_at.strftime('%b')
@@ -18,7 +9,7 @@ class EventDecorator < ApplicationDecorator
   end
 
   def guests_info_item
-    h.content_tag(:div, class: 'list-view-pf-additional-info-item') do
+    info_item_wrapper do
       h.concat h.fa_icon('users')
       h.concat h.content_tag(:strong, guests_count)
       h.concat Guest.model_name.human(count: guests_count)
@@ -26,7 +17,7 @@ class EventDecorator < ApplicationDecorator
   end
 
   def courses_info_item
-    h.content_tag(:div, class: 'list-view-pf-additional-info-item') do
+    info_item_wrapper do
       h.concat h.fa_icon('cutlery')
       h.concat h.content_tag(:strong, courses_count)
       h.concat Course.model_name.human(count: courses_count)
@@ -34,11 +25,18 @@ class EventDecorator < ApplicationDecorator
   end
 
   def dishes_info_item
-    h.content_tag(:div, class: 'list-view-pf-additional-info-item') do
+    info_item_wrapper do
       h.concat h.fa_icon('spoon')
       h.concat h.content_tag(:strong, dishes.count)
       h.concat Dish.model_name.human(count: dishes.count)
     end
   end
 
+  private
+
+  def info_item_wrapper
+    h.content_tag(:div, class: 'list-view-pf-additional-info-item') do
+      yield
+    end
+  end
 end
