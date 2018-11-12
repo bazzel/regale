b87a7015c298cf25651cb32aa4be112392714f8d
Patrick Baselier <patrickbaselier@Patricks-MacBook-Pro.local>
2018-11-12 20:30:25 +0100

    Reuse partial

--- config/initializers/eager_load.rb
+++
@@ -1,7 +0,0 @@
-if Rails.env.development? || Rails.env.test?
-  # To make `Dish.descendants` work.
-  Rails.application.config.eager_load_paths = Rails.root.join('app/models')
-  Rails.application.reloader.to_prepare do
-    Dir[Rails.root.join('app/models/*.rb')].each {|file| require_dependency file}
-  end
-end
