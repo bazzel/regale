c0c24a4c2172b7d62797f0f5f35f2635284fb7aa
Patrick Baselier <patrickbaselier@Patricks-MacBook-Pro.local>
2018-11-16 20:09:17 +0100

    Always require_dependency

--- config/initializers/preload_sti_models.rb
+++ config/initializers/preload_sti_models.rb
@@ -1,6 +1,5 @@
-if Rails.env.development?
+#if Rails.env.development?
   %w(soup appetizer main_course dessert).each do |c|
     require_dependency File.join("app","models","#{c}.rb")
   end
-end
-
+#end
