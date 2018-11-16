347589e2782eb2dd31c88874482d1bb248ae56a8
Patrick Baselier <patrickbaselier@Patricks-MacBook-Pro.local>
2018-11-16 20:27:44 +0100

    Preload

--- config/initializers/preload_sti_models.rb
+++ config/initializers/preload_sti_models.rb
@@ -1,5 +1,6 @@
 #if Rails.env.development?
-  %w(soup appetizer main_course dessert).each do |c|
-    require_dependency File.join("app","models","#{c}.rb")
-  end
+%w(soup appetizer main_course dessert).each do |c|
+  c.classify.constantize
+  #require_dependency File.join("app","models","#{c}.rb")
+end
 #end
