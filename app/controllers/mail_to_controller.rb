3d23a74ad5fd17846392a6f5a06faa8b8c2b9a70
Patrick Baselier <patrickbaselier@Patricks-MacBook-Pro.local>
2018-12-16 20:30:32 +0100

    Decode

--- app/controllers/mail_to_controller.rb
+++ app/controllers/mail_to_controller.rb
@@ -1,8 +1,9 @@
 class MailToController < ApplicationController
 
   def menu_choices
-    @event = authorize(Event.find(params[:event_id]), policy_class: MailToPolicy).decorate
-    @guests = @event.guests
+    @event = authorize(Event.find(params[:event_id]), policy_class: MailToPolicy)
+    @guests = @event.guests.includes(:soup, :appetizer, :main_course, :dessert)
+    @event = @event.decorate
 
     mailer = MailToMailer.with(event: @event, guests: @guests).menu_choices
     redirect_to mailer.mailto and return
--- config/initializers/extensions/action_mailer/message_delivery.rb
+++ config/initializers/extensions/action_mailer/message_delivery.rb
@@ -1,11 +1,20 @@
 module MailTo
   include ActionView::Helpers::UrlHelper
+  require 'htmlentities'
+
   REGEX = %r{.*\shref="(?<href>.*)".*}
 
   def mailto
-    link = mail_to(nil, 'Lorem', subject: subject, body: text_part.body.encoded)
+    link = mail_to(nil, 'Lorem', subject: subject, body: mail_to_body)
     matches = link.match(REGEX)
+
     matches[:href].gsub('&amp;', '&')
+  end
+
+  private
+  def mail_to_body
+    coder = HTMLEntities.new
+    coder.decode(text_part.body.encoded)
   end
 end
 
