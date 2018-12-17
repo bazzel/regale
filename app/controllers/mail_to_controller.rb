b69b5b2f80331e75ba25c99c5ebfc41f175c43b7
Patrick Baselier <patrickbaselier@patricks-mbp.eu.asml.com>
2018-12-17 06:42:53 +0100

    Compose mail (WIP)

--- app/controllers/mail_to_controller.rb
+++ app/controllers/mail_to_controller.rb
@@ -2,11 +2,15 @@ class MailToController < ApplicationController
 
   def menu_choices
     @event = authorize(Event.find(params[:event_id]), policy_class: MailToPolicy)
-    @guests = @event.guests.includes(:soup, :appetizer, :main_course, :dessert)
+    @guests = @event.guests.includes(:soup, :appetizer, :main_course, :dessert).where(accept_status: sanizited_accept_status_param)
     @event = @event.decorate
 
     mailer = MailToMailer.with(event: @event, guests: @guests).menu_choices
     redirect_to mailer.mailto and return
   end
 
+  private
+  def sanizited_accept_status_param
+    params[:accept_status].map { |status| status.blank? ? nil : status }
+  end
 end
